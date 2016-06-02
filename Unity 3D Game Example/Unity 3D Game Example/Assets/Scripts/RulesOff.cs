970017773f66da6cb73cc4dcbcd4d9cef1786c02
shadow46446 <saketarora88@gmail.com>
2016-06-02 08:49:29 -0700

    text!!!!!!
    got the text to work

---
+++ Unity 3D Game Example/Unity 3D Game Example/Assets/Scripts/RulesOff.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class RulesOff : MonoBehaviour {
+
+	public GameObject rules;
+
+	void OnTriggerEnter(Collider other) {
+		Rigidbody2D otherBody = other.GetComponent <Rigidbody2D> ();
+		Debug.Log ("Turn rules off");
+		if (rules.activeInHierarchy == true){
+			rules.SetActive (false);
+		}
+	}
+}
